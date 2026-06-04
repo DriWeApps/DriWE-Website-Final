685c0b39a52904c62b0e35ecaf152361dff34ca6
DriWeApps <cto@driwe.in>
2026-06-04 18:52:45 +0530

    add google auth logs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,3 @@
-// export { handler as GET, handler as POST };
-
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-
-//   callbacks: {
-//     async signIn({ profile }) {
-
-//       const email = profile?.email || "";
-
-//       const allowedEmails =
-//         process.env.ALLOWED_GOOGLE_EMAILS?.split(",") || [];
-
-//       return allowedEmails.includes(email);
-//     },
-//   },
-// });
-
-// export { handler as GET, handler as POST };
-
-
-
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -42,16 +12,14 @@ const handler = NextAuth({
   callbacks: {
     async signIn({ profile }) {
 
+      const allowedEmails = [
+        "cto@driwe.in",
+        "admin@driwe.in",
+        "imroz@driwe.in",
+        "affan@driwe.in",
+      ];
+
       const email = profile?.email || "";
-
-      console.log("Google email:", email);
-
-      const allowedEmails =
-        process.env.ALLOWED_GOOGLE_EMAILS
-          ?.split(",")
-          .map((e) => e.trim()) || [];
-
-      console.log("Allowed emails:", allowedEmails);
 
       return allowedEmails.includes(email);
     },
